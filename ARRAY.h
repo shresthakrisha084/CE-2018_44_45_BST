163ccf814c55e48a32089b8ac192ab3dff5b141b
shresthakrisha084 <52463129+shresthakrisha084@users.noreply.github.com>
2020-01-30 10:42:28 +0545

    Add files via upload

---
+++ ARRAY.h
@@ -0,0 +1,12 @@
+#include "BinaryTree.h"
+#define Max_SIZE 1000
+
+class ArrayBST::public BST
+{
+	public:
+		virtual void add(int data);
+			virtual void preorder traversal();
+			virtual	bool search();
+	private:
+		int data[MAX_SIZE];
+};
