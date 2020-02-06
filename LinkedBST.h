e03eade9bdd23eb26a6d01dad30f4adc0816e6c1
sthasam2 <sthasambeg2@outlook.com>
2020-02-06 11:09:47 +0545

    Created definitions
    Definitions added to LinkedBST.cpp, LinkedBST.h

--- LinkedBST.h
+++ LinkedBST.h
@@ -1,31 +1,52 @@
 #include "BST.h"
 
-class Node {
-	int data;
-	Node*  left;
-	Node*  right;
+class Node{
+	public:
+		//node data
+		int data;
+
+		//subtree/child pointers
+		Node*  left;
+		Node*  right;
+
+		//constructor
+		Node(){
+			data=0;
+			left=NULL;
+			right=NULL;
+		}
 };
 
-enum Traversal{
-	PreTVR,
-	PostTVR,
-	InTVR
-};
 
 class LinkedBST: public BST{
+	//private functions
 	private:
-		Node *root;
-		void add(Node* &root, int data);
+		//insert node to specific subtree
+		void insert(Node* &subtree, Node* newNode);
 		bool find(Node* &subtree, int targetKey);
-		void traverse(Node* root);
+		void traverse(Node* root, Traversal Type);
 	
+	//public function
 	public:
+		Node* root;
 		LinkedBST();
 
-		void add(int data);
-		void preOrderTraversal();
-		bool search(int data);
+		void add(int dataUser);
+		void add(Node* &root, int dataUser);
+
+		void preorderTraversal();
+		void postorderTraversal();
+		void inorderTraversal();
+		
+		bool search(int dataUser);
+		
 		int min();
 		int max();
 
+};
+
+enum Traversal{
+	PreTV,
+	PostTV,
+	InTV
 };
