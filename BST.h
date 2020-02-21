dee989c7bcbd1e5fe0d57710ec91862affcf657f
KRISHA SHRESTHA <shresthakrisha084@gmail.com>
2020-02-21 14:17:44 +0545

    error

--- BST.h
+++ BST.h
@@ -9,7 +9,7 @@ public:
     virtual bool search(int data) = 0;
 
     virtual int min() = 0;
-    virtual int max() = 0;virtual 
+    virtual int max() = 0; 
     virtual void inordertraversal() = 0;
     virtual void deleteitem(int data) = 0;
 
